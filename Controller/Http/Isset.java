6b8b07d618e0cd237f21e7604be567eafbd90e2d
Razvan <tangent.jotey@gmail.com>
2017-06-28 14:00:13 +0200

    Isset controller added for cookies and files

--- Controller/Http/Isset.java
+++ Controller/Http/Isset.java
@@ -6,6 +6,7 @@
 package javahttpserver.Controller.Http;
 
 import com.google.gson.JsonObject;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,36 +18,33 @@ import javahttpserver.JHS;
  *
  * @author Razvan
  */
-public class Get implements HttpInterface{
+public class Isset implements HttpInterface{
     @Override
     public void main(HttpEvent e, ArrayList<String> args,JsonObject post){
         e.send("No resource specified");
     }
     public void file(HttpEvent e, ArrayList<String> args,JsonObject post) throws FileNotFoundException, IOException{
-        e.setContentType(JHS.processContentType(args.get(0)));
-        e.sendFileContents("/"+(args.get(0).equals("")?args.get(1):args.get(0)));
-    }
-    
-    class Cookie{
-        String 
-                DataType,
-                Value;
-
-        public Cookie(String DataType,String Value) {
-            this.DataType=DataType;
-            this.Value=Value;
+        if(args.size() >= 0){
+            File f = new File(JHS.PUBLIC_WWW+"/"+args.get(0));
+            if(f.exists()){
+                e.send(0);
+            }else{
+                e.send(-2);
+            }
+        }else{
+            e.send(-1);
         }
-        
     }
     
     public void cookie(HttpEvent e, ArrayList<String> args,JsonObject post){
-        e.setContentType("application/json");
-        if(e.cookieIsset(args.get(0))){
-            String jsonCookie = JHS.JSON_PARSER.toJson(new Cookie("Cookie", e.getCookie(args.get(0))));
-            e.send(jsonCookie);
+        if(args.size() >= 0){
+            if(e.cookieIsset(args.get(0))){
+                e.send(0);
+            }else{
+                e.send(-2);
+            }
         }else{
-            e.setHeaderField("Status", "HTTP/1.1 404 Not Found");
-            e.send();
+            e.send(-1);
         }
     }
 }
