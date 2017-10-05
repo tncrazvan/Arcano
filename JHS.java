813910068260fd1b461faaa3a6350bcb8d20eb63
razvan <razvan@razvan-System-Product-Name>
2017-10-06 01:31:15 +0200

    Removed some unused libraries.

--- JHS.java
+++ JHS.java
@@ -19,8 +19,6 @@ import java.util.Arrays;
 import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javahttpserver.Http.HttpEvent;
 import javahttpserver.WebSocket.WebSocketEvent;
 
