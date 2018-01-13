aebaec74467f40d5d138aea1af59f449fc0a11fb
Razvan <tangent.jotey@gmail.com>
2018-01-13 06:53:13 +0100

    GPLv3 License added

--- ELK.java
+++ ELK.java
@@ -1,7 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * ElkServer is a Java library that makes it easier
+ * to program and manage a Java servlet by providing different tools
+ * such as:
+ * 1) An MVC (Model-View-Controller) alike design pattern to manage 
+ *    client requests without using any URL rewriting rules.
+ * 2) A WebSocket Manager, allowing the server to accept and manage 
+ *    incoming WebSocket connections.
+ * 3) Direct access to every socket bound to every client application.
+ * 4) Direct access to the headers of the incomming and outgoing Http messages.
+ * Copyright (C) 2016  Tanase Razvan Catalin
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package elkserver;
 import com.google.gson.Gson;
