e0a020663c8d9409b669d1a539fbcee0a4b57426
Razvan <tangent.jotey@gmail.com>
2018-01-20 17:40:34 +0100

    WebSocketManager now correctly detects socket closure for both mozilla and webkit based browsers

--- WebSocket/WebSocketManager.java
+++ WebSocket/WebSocketManager.java
@@ -178,7 +178,8 @@ public abstract class WebSocketManager extends EventManager{
         // this is the standard way that all browsers use 
         // to indicate and end connection frame
         // I make sure there is no
-        if(payload[0] == -120){
+        //System.out.println("payload[0]:"+payload[0]+",bytes:"+bytes);
+        if(payload[0] == -120 && bytes <= 6){
             close();
             return false;
         }else if(bytes <= 6){
