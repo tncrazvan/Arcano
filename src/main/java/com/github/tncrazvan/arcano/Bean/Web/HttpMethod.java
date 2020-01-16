badc381c741da0f98586305b3ee379c0d5996384
Razvan <tangent.jotey@gmail.com>
2020-01-16 01:05:28 +0100

    update

--- src/main/java/com/github/tncrazvan/arcano/Bean/Web/HttpMethod.java
+++ src/main/java/com/github/tncrazvan/arcano/Bean/Web/HttpMethod.java
@@ -16,5 +16,5 @@ import java.lang.annotation.Target;
 @Retention(value = RetentionPolicy.RUNTIME)
 @Documented
 public @interface HttpMethod {
-    public String name();
+    public String name() default "GET";
 }
