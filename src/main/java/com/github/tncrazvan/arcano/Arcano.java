f154bd0121d754b9e5fce7861db55f09ec2f8489
Razvan <tangent.jotey@gmail.com>
2020-07-04 17:12:20 +0200

    Update Arcano.java

--- src/main/java/com/github/tncrazvan/arcano/Arcano.java
+++ src/main/java/com/github/tncrazvan/arcano/Arcano.java
@@ -41,215 +41,191 @@ import com.github.tncrazvan.arcano.websocket.WebSocketEventManager;
  * @author Razvan Tanase
  */
 public class Arcano extends SharedObject {
-    private static SmtpServer smtpServer;
-    public static void main (final String[] args) throws IOException, ClassNotFoundException, NoSuchAlgorithmException, URISyntaxException{
-        if(args.length == 0) {
+    // private static SmtpServer smtpServer;
+    public static void main(final String[] args)
+            throws IOException, ClassNotFoundException, NoSuchAlgorithmException, URISyntaxException {
+        if (args.length == 0) {
             System.out.println("No arguments provided. Server won't start.");
             return;
         }
         new Arcano().listen(args);
     }
 
-    
-    /**
-     * Recursive method used to find all classes in a given directory and subdirs.
-     * Source: https://stackoverflow.com/questions/520328/can-you-find-all-classes-in-a-package-using-reflection#answer-520344
-     * @param directory   The base directory
-     * @param packageName The package name for classes found inside the base directory
-     * @return The classes
-     * @throws ClassNotFoundException
-     */
-    private static List<Class<?>> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        List<Class<?>> classes = new ArrayList<>();
-        if (!directory.exists()) {
-            return classes;
-        }
-        File[] files = directory.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                assert !file.getName().contains(".");
-                classes.addAll(findClasses(file, packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class")) {
-                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-            }
-        }
-        return classes;
-    }
-    
-    /**
-    * Scans all classes accessible from the context class loader which belong to the given package and subpackages.
-    * Source: https://stackoverflow.com/questions/520328/can-you-find-all-classes-in-a-package-using-reflection#answer-520344
-    * @param packageName The base package
-    * @return The classes
-    * @throws ClassNotFoundException
-    * @throws IOException
-    */
-   private static Class<?>[] getClasses(Package pckg) throws ClassNotFoundException, IOException {
-       String packageName = pckg.getName();
-       ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-       assert classLoader != null;
-       String path = packageName.replace('.', '/');
-       Enumeration<URL> resources = classLoader.getResources(path);
-       List<File> dirs = new ArrayList<>();
-       while (resources.hasMoreElements()) {
-           URL resource = resources.nextElement();
-           dirs.add(new File(resource.getFile()));
-       }
-       ArrayList<Class<?>> classes = new ArrayList<>();
-       for (File directory : dirs) {
-           classes.addAll(findClasses(directory, packageName));
-       }
-       return classes.toArray(new Class<?>[classes.size()]);
-   }
-    
-    
     /**
      * Make a new Arcano Server
      * 
      * @param pckg The package that contains your services.<br />
-     * The package must be contained inside the current project's directory layout.
+     *             The package must be contained inside the current project's
+     *             directory layout.
      */
-    public Arcano(){}
-    
+    public Arcano() {
+    }
+
     /**
      * Starts the server listening.
      * 
-     * @param args First argument must be the settings file. Check documentation to
-     *             learn how to create a settings files.
+     * @param args   First argument must be the settings file. Check documentation
+     *               to learn how to create a settings files.
      * @param action Action to be run before each connection.
      */
     public final void listen(String[] args) {
         System.out.println("ARGS: " + Arrays.toString(args));
-
+        System.out.println("Caller Working Directory: " + config.callerDir);
+        System.out.println("Config Working Directory: " + config.dir);
         try {
             config.parse(args[0], this, args);
+            configureThreadPoolPolicy();
+            configureSmtpPolicy();
+            pushWebSocketCommits();
 
-            switch(config.threads.policy){
-                case Threads.POLICY_FIX:
-                    executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(config.threads.pool);
-                    break;
-                case Threads.POLICY_CACHE:
-                    executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
-                    break;
-                case Threads.POLICY_STEAL:
-                default:
-                    if(config.threads.pool == 0)
-                        service = Executors.newWorkStealingPool();
-                    else
-                        service = Executors.newWorkStealingPool(config.threads.pool);
-                    break;
-            }
-
-
-            System.out.println("Caller Working Directory: "+config.callerDir);
-            System.out.println("Config Working Directory: "+config.dir);
-
-            if(config.smtp.enabled)
-                if (!config.smtp.hostname.equals("")) {
-                    smtpServer = new SmtpServer(new ServerSocket(), this,config.smtp.bindAddress, config.smtp.port,
-                            config.smtp.hostname);
-                    new Thread(smtpServer).start();
-                } else {
-                    System.err.println("\n[WARNING] smtp.hostname is not defined. Smtp server won't start. [WARNING]");
+            if (!config.certificate.name.equals(""))
+                try {
+                    configureSecureServer();
+                } catch (KeyStoreException | CertificateException | UnrecoverableKeyException
+                        | KeyManagementException ex) {
+                    LOGGER.log(Level.SEVERE, null, ex);
+                } catch (NoSuchAlgorithmException ex) {
+                    Logger.getLogger(Arcano.class.getName()).log(Level.SEVERE, null, ex);
                 }
-            
-            //push websocket commits
-            Runnable webSocketPushRunnable = () -> {
-                this.oldWebSocketEventManager.clear();
-                int available = 0;
-                Collection<WebSocketEventManager> collection;
-                while(true){
-                    collection = this.webSocketEventManager.values();
-                    for (WebSocketEventManager manager : collection) {
-                        if(!manager.isConnected()){
-                            this.oldWebSocketEventManager.add(manager);
-                            continue;
-                        }
-                        InputStream read = manager.getRead();
-                        if(read != null){
-                            try {
-                                if((available = read.available()) > 0 && manager.isConnected()){
-                                    manager.unmask(read.readNBytes(available<config.webSocket.mtu?available:config.webSocket.mtu));
-                                }
-                                if(manager.getCommits().size() > 0){
-                                    for (WebSocketCommit commit : manager.getCommits()) {
-                                        manager.push(commit);
-                                    }
-                                }
-                            } catch (IOException ex) {
-                                this.oldWebSocketEventManager.add(manager);
-                                LOGGER.log(Level.SEVERE, null, ex);
-                            }
-                        }
-                    }
-                    
-                    //remove old events
-                    for (WebSocketEventManager element : this.oldWebSocketEventManager) {
-                        this.webSocketEventManager.remove(element.getUuid());
-                    }
-                    this.oldWebSocketEventManager.clear();
-                }
-            };
-            new Thread(webSocketPushRunnable).start();
-            
-            if (!config.certificate.name.equals("")) try {
-                final char[] password = config.certificate.password.toCharArray();
-                final KeyStore keyStore = KeyStore.getInstance(new File(config.dir + "/" + config.certificate.name),password);
-                final TrustManagerFactory trustManagerFactory = TrustManagerFactory
-                        .getInstance(TrustManagerFactory.getDefaultAlgorithm());
-                trustManagerFactory.init(keyStore);
-                final KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("NewSunX509");
-                keyManagerFactory.init(keyStore, password);
-                final SSLContext context = SSLContext.getInstance("TLSv1.2");// "SSL" "TLS"
-                context.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
-
-                final SSLServerSocketFactory factory = context.getServerSocketFactory();
-                try (SSLServerSocket ss = ((SSLServerSocket) factory.createServerSocket())) {
-                    ss.setSoTimeout(this.config.timeout);
-                    ss.bind(new InetSocketAddress(config.bindAddress, config.port));
-                    HttpRequestReader reader;
-                    System.out.println("Server started (using TLSv1.2).");
-                    while (config.listen) {
-                        try{
-                            reader = new HttpRequestReader(this, ss.accept());
-                            if(executor == null)
-                                service.submit(reader);
-                            else
-                                executor.submit(reader);
-                        }catch(SocketTimeoutException e){
-                            LOGGER.log(Level.SEVERE, null, e);
-                            //System.out.println(String.format("Socket timed out after %s milliseconds.", this.config.timeout));
-                        }
-                    }
-                }
-            } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | KeyManagementException ex) {
-                LOGGER.log(Level.SEVERE, null, ex);
-            } catch (NoSuchAlgorithmException ex) {
-                Logger.getLogger(Arcano.class.getName()).log(Level.SEVERE, null, ex);
-            } else {
-                ServerSocket ss = new ServerSocket();
-                //ss.setSoTimeout(this.config.timeout);
-                ss.bind(new InetSocketAddress(config.bindAddress, config.port));
-                HttpRequestReader reader;
-                System.out.println("Server started.");
-                while (config.listen) {
-                    try {
-                        reader = new HttpRequestReader(this, ss.accept());
-                        if(executor == null)
-                            service.submit(reader);
-                        else
-                            executor.submit(reader);
-                    }catch(SocketTimeoutException e){
-                        LOGGER.log(Level.SEVERE, null, e);
-                        //System.out.println(String.format("Socket timed out after %s milliseconds.", this.config.timeout));
-                    }catch (NoSuchAlgorithmException ex) {
-                        LOGGER.log(Level.SEVERE, null, ex);
-                    }
-                }
-                ss.close();
+            else {
+                configureServer();
             }
         } catch (IOException ex) {
             LOGGER.log(Level.SEVERE, null, ex);
         }
+    }
+
+    private void configureServer() throws IOException {
+        ServerSocket ss = new ServerSocket();
+        // ss.setSoTimeout(this.config.timeout);
+        ss.bind(new InetSocketAddress(config.bindAddress, config.port));
+        HttpRequestReader reader;
+        System.out.println("Server started.");
+        while (config.listen) {
+            try {
+                reader = new HttpRequestReader(this, ss.accept());
+                if (executor == null)
+                    service.submit(reader);
+                else
+                    executor.submit(reader);
+            } catch (SocketTimeoutException e) {
+                LOGGER.log(Level.SEVERE, null, e);
+                // System.out.println(String.format("Socket timed out after %s milliseconds.",
+                // this.config.timeout));
+            } catch (NoSuchAlgorithmException ex) {
+                LOGGER.log(Level.SEVERE, null, ex);
+            }
+        }
+        ss.close();
+    }
+
+    private void configureSecureServer() throws KeyStoreException, NoSuchAlgorithmException, CertificateException,
+            IOException, KeyManagementException, UnrecoverableKeyException {
+        final char[] password = config.certificate.password.toCharArray();
+        final KeyStore keyStore = KeyStore.getInstance(new File(config.dir + "/" + config.certificate.name), password);
+        final TrustManagerFactory trustManagerFactory = TrustManagerFactory
+                .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(keyStore);
+        final KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("NewSunX509");
+        keyManagerFactory.init(keyStore, password);
+        final SSLContext context = SSLContext.getInstance("TLSv1.2");// "SSL" "TLS"
+        context.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+
+        final SSLServerSocketFactory factory = context.getServerSocketFactory();
+        try (SSLServerSocket ss = ((SSLServerSocket) factory.createServerSocket())) {
+            ss.setSoTimeout(this.config.timeout);
+            ss.bind(new InetSocketAddress(config.bindAddress, config.port));
+            HttpRequestReader reader;
+            System.out.println("Server started (using TLSv1.2).");
+            while (config.listen) {
+                try {
+                    reader = new HttpRequestReader(this, ss.accept());
+                    if (executor == null)
+                        service.submit(reader);
+                    else
+                        executor.submit(reader);
+                } catch (SocketTimeoutException e) {
+                    LOGGER.log(Level.SEVERE, null, e);
+                    // System.out.println(String.format("Socket timed out after %s milliseconds.",
+                    // this.config.timeout));
+                }
+            }
+        }
+    }
+
+    private void configureSmtpPolicy() throws IOException {
+        if (config.smtp.enabled)
+            if (!config.smtp.hostname.equals("")) {
+                SmtpServer smtpServer = new SmtpServer(new ServerSocket(), this, config.smtp.bindAddress,
+                        config.smtp.port, config.smtp.hostname);
+                new Thread(smtpServer).start();
+            } else {
+                System.err.println("\n[WARNING] smtp.hostname is not defined. Smtp server won't start. [WARNING]");
+            }
+    }
+
+    private void configureThreadPoolPolicy() {
+        switch (config.threads.policy) {
+            case Threads.POLICY_FIX:
+                executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(config.threads.pool);
+                break;
+            case Threads.POLICY_CACHE:
+                executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+                break;
+            case Threads.POLICY_STEAL:
+            default:
+                if (config.threads.pool == 0)
+                    service = Executors.newWorkStealingPool();
+                else
+                    service = Executors.newWorkStealingPool(config.threads.pool);
+                break;
+        }
+    }
+
+    private void pushWebSocketCommits() {
+        // push websocket commits
+        Runnable webSocketPushRunnable = () -> {
+            this.oldWebSocketEventManager.clear();
+            int available = 0;
+            Collection<WebSocketEventManager> collection;
+            while (true) {
+                collection = this.webSocketEventManager.values();
+                for (WebSocketEventManager manager : collection) {
+                    if (!manager.isConnected()) {
+                        this.oldWebSocketEventManager.add(manager);
+                        continue;
+                    }
+                    InputStream read = manager.getRead();
+                    if (read != null) {
+                        try {
+                            if ((available = read.available()) > 0 && manager.isConnected()) {
+                                manager.unmask(read.readNBytes(
+                                        available < config.webSocket.mtu ? available : config.webSocket.mtu));
+                            }
+                            if (manager.getCommits().size() > 0) {
+                                for (WebSocketCommit commit : manager.getCommits()) {
+                                    manager.push(commit);
+                                }
+                            }
+                        } catch (IOException ex) {
+                            this.oldWebSocketEventManager.add(manager);
+                            LOGGER.log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+
+                // remove old events
+                for (WebSocketEventManager element : this.oldWebSocketEventManager) {
+                    this.webSocketEventManager.remove(element.getUuid());
+                }
+                this.oldWebSocketEventManager.clear();
+                try {
+                    Thread.sleep(0, 1);
+                } catch (InterruptedException ex) {
+                    LOGGER.log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+        new Thread(webSocketPushRunnable).start();
     }
 }
